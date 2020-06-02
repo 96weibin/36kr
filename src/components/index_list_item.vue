49a71a74b857e33673296b9d371aefb934d98877
weibinZjz <1643960119@qq.com>
2020-06-02 10:01:33 +0800

    img写服务器全地址修改

--- src/components/index_list_item.vue
+++ src/components/index_list_item.vue
@@ -55,7 +55,7 @@ export default {
   },
   methods: {
     getImgUrl(data){
-      return `./img?src=${data.src}`;
+      return `http://111.229.241.56:8090/img?src=${data.src}`;
     },
   },
   filters : {
