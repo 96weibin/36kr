0783db0d35bed37b1a547ce3ab49e4ae278acf28
weibinZjz <1643960119@qq.com>
2020-06-02 09:48:01 +0800

    修改img路径

--- src/components/index_list_item.vue
+++ src/components/index_list_item.vue
@@ -55,7 +55,7 @@ export default {
   },
   methods: {
     getImgUrl(data){
-      return `http://111.229.241.56/static/img?src=${data.src}`;
+      return `./img?src=${data.src}`;
     },
   },
   filters : {
