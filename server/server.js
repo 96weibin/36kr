93743697d680d3a2a1ad3b480262fdd464bb95ae
weibinZjz <1643960119@qq.com>
2020-06-02 11:27:25 +0800

    koa访问服务

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const Koa = require('koa')
 const koaStaicCache = require('koa-static-cache')
+const router = require('koa-router')()
+const fs = require('fs')
 let {resolve} = require('path')
 let server = new Koa();
 server.listen(8081);
+
 
 server.use(koaStaicCache(resolve('../dist')),{
     maxAge : 60 * 60 * 24 ,
     gzip : true
 })
+
+server.use(router.routes())
+router.get('/',async(ctx, next)=>{
+    let f1 = fs.readFileSync('../dist/index.html')
+    ctx.response.body = f1.toString()
+    //流读取   pipe到 ctx.res不知道为什么不好使
+})
+
 
